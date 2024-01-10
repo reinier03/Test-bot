7f9e054b7a01d5440df0064b8a7f033ea5a97c9a
Reima <reiniermayea@gmail.com>
2024-01-09 21:45:58 -0500

    hola

---
+++ test.py
@@ -0,0 +1,41 @@
+import telebot 
+from flask import Flask, request
+from threading import Thread
+
+bot=telebot.TeleBot("6529482461:AAGiVcX3FZj_9e6WRD0QLgRmgbnCNvIzgqA")
+
+bot.send_message(1413725506, "HOLA")
+
+
+@bot.message_handler(commands=["start", "help"])
+def cmd_start(message):
+  if not message.chat.type == "private":
+    return
+  bot.send_message(
+      message.chat.id,
+      "Hola! 😁, Bienvenido a <b>Last Botonera</b> bot.\nAquí se encuentran los canales afiliados a la botonera y por ende, a <a href='https://t.me/LastHopePosting'>Last Hope</a>\n\n\n<u>Los comandos disponibles (por ahora) son</u>:\n\n/mostrar Si quiere SOLICITAR los CANALES de la Botonera e <b>Información</b> sobre el tiempo restante de la PRÓXIMA PUBLICACIÓN de dicha botonera en sus CANALES afiliados\n\n/ingresar Si quiere INGRESAR su CANAL EN la BOTONERA\n\n/start o /help Para mostrar ESTE mensaje de ayuda\n\n\n\n<u>Nota:</u>\nSi quiere notificar algo o tiene alguna duda consulte con mi creador y guapetón propietario de Last Hope ( ͡° ͜ʖ ͡°)\n\n👉<a href='https://t.me/mistakedelalaif'>Reima</a>👈",
+      parse_mode="html",
+      disable_web_page_preview=True)
+  #/promocionar LA MÁS FACHERA utilidad de este bot, escucha bien pepillo 🦻\nCon este comando puedes hacer pxp (promocion de tu canal a través de otro owner como tú) sin tener que acosar por privado al pobre muchacho (o muchacha, ve a ver tú) (¬‿¬) Simplemente me envias tu publicación de promoción, yo le pido que confirme si quiere hacer el pxp contigo y si acepta, directamente publico las promos en los respectivos canales. Como requisitos indispensables es que tanto tú como él me tienen que dar admin y permisos para publicar, el otro requisito es que ambos ya hayan hablado conmigo y no me tengan bloqueado ¿Qué esperas para comenzar? ಠ_ಠ
+
+
+server_address = ""
+app = ""
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return f"Hello, The bot is running in {request.host_url}"
+
+
+def runServer():
+  app.run(host='0.0.0.0', port=8181)
+
+
+t = Thread(name="run_server", target=runServer)
+t.start()
+
+
+bot.infinity_polling()
